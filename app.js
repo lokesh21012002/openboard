4413e6c72cb6dd27d1f5b01b7ea865efdb446e75
lokesh21012002 <gawandelokesh123@gmail.com>
2021-10-28 07:30:00 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+app.use(express.static("public"));
+
+const PORT = 5000;
+ let server=app.listen(PORT, () => {
+    console.log("server started");
+ })
+let io = socket(server);
+io.on('connection', (socket) => {
+    console.log("made socket connection");
+    socket.on("beginPath", (data) => {
+        io.sockets.emit("beginPath", data);
+        
+    })
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+
+
+    })
+    socket.on("redoUndo", (data) => {
+        io.socket.emit("redoUndo", data);
+
+    })
+})
